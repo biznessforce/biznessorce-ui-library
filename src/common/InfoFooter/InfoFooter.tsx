61b46b147f78ee7d24ecf53dd938d91d01aa8967
Abdul Fasith <abdul.fasith@biznessforce.com>
2025-10-28 15:22:20 +0530

    refactor: improve formatting and structure in InfoFooter component

--- src/common/InfoFooter/InfoFooter.tsx
+++ src/common/InfoFooter/InfoFooter.tsx
@@ -15,31 +15,45 @@ type InfoFooterProps = {
   };
 };
 
-const InfoFooter = ({ info_1, info_2 }: InfoFooterProps) => {
+const InfoFooter = ({
+  info_1,
+  info_2,
+}: InfoFooterProps) => {
   return (
     <>
-      <Flex vertical className="my-3" gap={10}>
+      <Flex
+        vertical
+        className="my-3"
+        gap={10}>
         <Space>
-          <Typography.Text strong type="secondary">
+          <Typography.Text
+            strong
+            type="secondary">
             {info_1.title}
           </Typography.Text>
-          <Typography.Text strong>{info_1.userBy}</Typography.Text>
+          <Typography.Text strong>
+            {info_1.userBy}
+          </Typography.Text>
           {info_1.date && (
             <Typography.Text strong>
-              on {info_1.date.format("DD-MMM-YYYY, h:mm a")}
+              on {info_1.date.format("DD-MMM-YYYY HH:mm")}
             </Typography.Text>
           )}
         </Space>
 
         {info_2 && (
           <Space>
-            <Typography.Text strong type="secondary">
+            <Typography.Text
+              strong
+              type="secondary">
               {info_2.title}
             </Typography.Text>
-            <Typography.Text strong>{info_2.userBy}</Typography.Text>
+            <Typography.Text strong>
+              {info_2.userBy}
+            </Typography.Text>
             {info_2.date && (
               <Typography.Text strong>
-                on {info_2.date.format("DD-MMM-YYYY, h:mm a")}
+                on {info_2.date.format("DD-MMM-YYYY HH:mm")}
               </Typography.Text>
             )}
           </Space>
